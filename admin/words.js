3de688cc2947e6956ff983f2718ef704d25a3ac3
Zefau <Zefau@users.noreply.github.com>
2018-12-06 23:10:49 +0100

    Add files via upload

--- admin/words.js
+++ admin/words.js
@@ -5,9 +5,9 @@ systemDictionary = {
 		"en": "Devices",
 		"de": "Geräte"
 	},
-	"tab_alexa": {
-		"en": "Alexa integration",
-		"de": "Alexa Integration"
+	"tab_config": {
+		"en": "Configuration",
+		"de": "Einstellungen"
 	},
 	"tab_info": {
 		"en": "Information",
@@ -28,5 +28,45 @@ systemDictionary = {
     "enabled": {
 		"en": "Enabled",
 		"de": "Aktiviert"
+	},
+    "alexa": {
+		"en": "Alexa Voice Output",
+		"de": "Alexa Sprachausgabe"
+	},
+    "telegram": {
+		"en": "Telegram Notification",
+		"de": "Telegram Benachrichtigung"
+	},
+    "alexa_info": {
+		"en": "Please specify ID of the Alexa device you want to be informed by (leave empty to not say anything)",
+		"de": "Bitte ID des Alexa Geräts angeben, das benachrichten soll (leer lassen, um keine Benachrichtigung zu anzusagen)"
+	},
+    "telegram_info": {
+		"en": "Please specificy users (comma separated) that shall be informed (or enter &quot;ALL&quot; if all users shall be informed; leave empty to not send any message)",
+		"de": "Bitte Benutzer angeben (mit Komma separiert), die benachrichtigt werden sollen (oder &quot;ALL&quot;, wenn alle Benutzer informiert werden sollen; leer lassen, um keine Nachricht zu schicken)"
+	},
+    "alexaPhrase": {
+		"en": "You may specificy phrases for Alexa to say when a device starts or finishes. Use %device% (including %) to use name of device.",
+		"de": "Die folgenden Ausdrücke sagt Alexa, wenn ein Gerät startet oder fertig ist. Benutze %device% (mit %), um den Namen des Geräts zu verwenden."
+	},
+    "alexaStarted": {
+		"en": "Alexa says this phrase when device starts",
+		"de": "Alexa sagt diesen Ausdruck, wenn das Gerät startet"
+	},
+    "alexaFinished": {
+		"en": "Alexa says this phrase when device finishes",
+		"de": "Alexa sagt diesen Ausdruck, wenn das Gerät fertig ist"
+	},
+    "telegramPhrase": {
+		"en": "You may specificy phrases for Telegram to message when a device starts or finishes. Use %device% (including %) to use name of device.",
+		"de": "Die folgenden Ausdrücke verschickt Telegram, wenn ein Gerät startet oder fertig ist. Benutze %device% (mit %), um den Namen des Geräts zu verwenden."
+	},
+    "telegramStarted": {
+		"en": "Telegram sends this message when device starts",
+		"de": "Telegram verschickt diese Nachricht, wenn das Gerät startet"
+	},
+    "telegramFinished": {
+		"en": "Telegram sends this message when device finishes",
+		"de": "Telegram verschickt diese Nachricht, wenn das Gerät fertig ist"
 	},
 };
