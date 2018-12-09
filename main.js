f3085247d9dece11658cd0cd9fe80e0bf2be23fc
Zefau <Zefau@users.noreply.github.com>
2018-12-09 17:36:49 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,42 @@
 'use strict';
 const utils = require(__dirname + '/lib/utils'); // Get common adapter utils
 const adapter = utils.Adapter('meter-sockets');
+const schedule = require('node-schedule');
 
 
 /*
  * internal libraries
  */
-const Library = require(__dirname + '/library.js');
+const Library = require(__dirname + '/lib/library.js');
 
 
 /*
  * variables initiation
  */
 var library = new Library(adapter);
+var schedules = [];
 var nodes = [
 	{'node': 'device', 'description': 'Device name', 'role': 'text'},
 	{'node': 'jobs', 'description': 'Completed jobs', 'role': 'list'},
 	{'node': '_running', 'description': 'Boolean if event is currently running', 'role': 'state', 'type': 'boolean'},
 	{'node': 'state', 'description': 'State of device to meter', 'role': 'text'},
 	{'node': 'enabled', 'description': 'Boolean if device metering is enabled', 'role': 'state', 'type': 'boolean'},
-	{'node': 'status.started', 'description': 'Timestamp the current event has started', 'role': 'value'},
-	{'node': 'status.startedDateTime', 'description': 'Datetime the current event has started', 'role': 'text'},
-	{'node': 'status.finished', 'description': 'Timestamp the last event has finished', 'role': 'value'},
-	{'node': 'status.finishedDateTime', 'description': 'Datetime the last event has finished', 'role': 'text'},
-	{'node': 'status.average', 'description': 'Current average usage', 'role': 'value'},
-	{'node': 'status.total', 'description': 'Total consumption on current run', 'role': 'value'},
-	{'node': 'status.threshold', 'description': 'Defined threshold of device', 'role': 'value'},
+	
+	{'node': 'job', 'description': 'Status of current job run', 'type': 'channel'},
+	{'node': 'job.started', 'description': 'Timestamp the current event has started', 'role': 'value'},
+	{'node': 'job.startedDateTime', 'description': 'Datetime the current event has started', 'role': 'text'},
+	{'node': 'job.finished', 'description': 'Timestamp the last event has finished', 'role': 'value'},
+	{'node': 'job.finishedDateTime', 'description': 'Datetime the last event has finished', 'role': 'text'},
+	{'node': 'job.threshold', 'description': 'Defined threshold of device', 'role': 'value.power.consumption', 'common': {'unit': 'Wh'}},
+	{'node': 'job.usage', 'description': 'Total usage of current job', 'role': 'value.power.consumption', 'common': {'unit': 'kWh'}},
+	
+	{'node': 'usage', 'description': 'Usage statistics of device', 'type': 'channel'},
+	{'node': 'usage._average', 'description': 'Current average usage', 'role': 'value.power.consumption', 'common': {'unit': 'Wh'}},
+	{'node': 'usage.yearly', 'description': 'Usage in current year', 'role': 'value.power.consumption', 'common': {'unit': 'kWh'}},
+	{'node': 'usage.quartely', 'description': 'Usage in current quarter', 'role': 'value.power.consumption', 'common': {'unit': 'kWh'}},
+	{'node': 'usage.monthly', 'description': 'Usage in current month', 'role': 'value.power.consumption', 'common': {'unit': 'kWh'}},
+	{'node': 'usage.daily', 'description': 'Usage in current day', 'role': 'value.power.consumption', 'common': {'unit': 'kWh'}},
+	{'node': 'costs', 'description': 'Cost statistics of device', 'type': 'channel'},
 ];
 
 
@@ -37,6 +48,7 @@ adapter.on('unload', function(callback)
 {
     try
 	{
+		schedules.forEach(function(schedule) {schedule.cancel()});
         adapter.log.info('Adapter stopped und unloaded.');
         callback();
     }
@@ -64,38 +76,115 @@ adapter.on('ready', function()
 	var devices = [];
 	adapter.config.devices.forEach(function(device, i)
 	{
-		if (device.state !== '')
+		device.id = i;
+		adapter.getForeignState(device.state, function(err, state)
 		{
-			device.id = i;
-			devices.push(createDevice(device));
-			adapter.subscribeForeignStates(device.state);
-		}
+			if (device.state === '' || err !== null || state === null)
+				adapter.log.warn(device.state === '' ? 'State of device ' + device.name + ' not set!' : 'State (' + device.state + ') of device ' + device.name + ' not found!');
+			
+			else
+				devices.push(createDevice(device)); // be aware that this is asynchronous
+		});
 	});
 	
-	// list devices
-	library.set({node: '_devices', description: 'List of devices', 'role': 'list'}, JSON.stringify(devices));
+	// update states
+	schedules.push(schedule.scheduleJob('0 * * * * *', function(date) // second, minute, hour, day of month, month, day of week
+	{
+		//adapter.log.silly('Writing power data to history states.');
+		
+		devices.forEach(function(device)
+		{
+			// write average value
+			var total, current = 0;
+			adapter.getState(device.id + '.usage._average', function(err, avg)
+			{
+				current = parseFloat((avg.val/1000).toFixed(3));
+				
+				// get date values
+				var year = date.getFullYear();
+				var quarter = '0' + Math.floor((date.getMonth()+3)/3);
+				var month = ('0' + (date.getMonth()+1)).substr(-2);
+				var day = ('0' + date.getDate()).substr(-2);
+				
+				// define states
+				var usage = ['daily', 'monthly', 'quartely', 'yearly'].map(function(i) {return '.usage.' + i});
+				var history = [year + '.total', year + '.Q' + quarter, year + '.' + month + '.total', year + '.' + month + '.' + day + '.total'].map(function(i) {return '.usage.history.' + i});
+				
+				// reset current values
+				var dates = {daily: day, monthly: month, quartely: quarter, yearly: year};
+				usage.forEach(function(state)
+				{
+					var node = device.id + state;
+					adapter.getObject(node, function(err, obj)
+					{
+						var scope = obj.common !== undefined && obj.common.scope !== undefined ? obj.common.scope: 0;
+						var index = state.substr(state.lastIndexOf('.')+1);
+						
+						if (scope != dates[index])
+						{
+							adapter.log.debug('Resetted ' + index + ' (' + node + ').');
+							adapter.extendObject(node, {common: {scope: dates[index]}});
+							adapter.setState(node, 0);
+						}
+					});
+				});
+				
+				// write value to current & history
+				['.job.usage'].concat(usage, history).forEach(function(state)
+				{
+					var node = device.id + state;
+					adapter.getState(node, function(err, value)
+					{
+						total = parseFloat(value.val.toFixed(3)) + current;
+						adapter.log.silly('Writing power usage of device ' + device.name + ' (+' + current + ' = ' + total + ') to history state -' + state + '-.');
+						
+						library.set(
+							{node: node, description: state, 'role': 'value.power.consumption', 'common': {'unit': 'kWh'}},
+							total
+						);
+					});
+				});
+				
+				
+			});
+		});
+	}));
+	
+	// meter devices
+	schedules.push(schedule.scheduleJob('*/10 * * * * *', function()
+	{
+		library.set({node: '_devices', description: 'List of devices', 'role': 'list'}, JSON.stringify(devices));
+		devices.forEach(function(device) {meter(device)});
+	}));
 });
 
-/*
- * STATE CHANGE
+
+/**
+ * Metering device
+ *
  *
  */
-adapter.on('stateChange', function(state, payload)
+function meter(device, settings)
 {
-	//adapter.log.debug('Changed state of ' + state + ': ' + JSON.stringify(payload));
-	
-	var value = Math.floor(payload.val*100)/100;
+	settings = settings !== undefined ? settings : {};
+	var max = settings.max || 6;
 	var average = 0;
-	var max = 12;
 	
-	var device, running, metered, average;
-	adapter.getForeignObject(state, function(err, obj)
+	var running, metered, average;
+	adapter.getForeignState(device.state, function(err, payload)
 	{
 		// error
 		if (err) return;
 		
-		// get device
-		device = obj.common['meter-sockets'];
+		// get power value
+		var value = Math.floor(payload.val*100)/100;
+		adapter.log.silly('Device ' + device.name + ' (' + device.state + ') reports usage of ' + value + '.');
+		
+		// set data
+		library._setValue(device.id + '.device', device.name);
+		library._setValue(device.id + '.state', device.state);
+		library._setValue(device.id + '.enabled', device.active);
+		library._setValue(device.id + '.job.threshold', device.threshold);
 		
 		// skip if device is disabled
 		if (device.active !== true) return;
@@ -106,17 +195,12 @@ adapter.on('stateChange', function(state, payload)
 			// error
 			if (err || !status) return;
 			
-			// set data
-			library._setValue(device.id + '.device', device.name);
-			library._setValue(device.id + '.state', device.state);
-			library._setValue(device.id + '.enabled', device.active);
-			
 			// set status
 			running = !!status.val;
 			library._setValue(device.id + '._running', running);
 			
 			// get metered data
-			adapter.getObject(device.id + '.status.average', function(err, obj)
+			adapter.getObject(device.id + '.usage._average', function(err, obj)
 			{
 				// get metered data
 				metered = obj.common.metered !== undefined ? JSON.parse(obj.common.metered) : [];
@@ -124,26 +208,22 @@ adapter.on('stateChange', function(state, payload)
 				// modify metered data and save
 				metered.push(value);
 				if (metered.length > max) metered.shift(); 
-				adapter.extendObject(device.id + '.status.average', {common: {metered: JSON.stringify(metered)}});
-				library._setValue(device.id + '.status.threshold', device.threshold);
+				adapter.extendObject(device.id + '.usage._average', {common: {metered: JSON.stringify(metered)}});
 				
 				// get average
 				average = getAverage(metered);
-				library._setValue(device.id + '.status.average', average);
-				
-				// add up total
-				adapter.getState(device.id + '.status.total', function(err, obj) {library._setValue(device.id + '.status.total', obj !== undefined ? parseInt(obj.val)+value : 0)});
+				library._setValue(device.id + '.usage._average', average);
 				
 				// set device running
 				if (running === false && average > device.threshold)
 				{
 					adapter.log.info('Device ' + device.name + ' has been detected started.');
 					library._setValue(device.id + '._running', true);
-					library._setValue(device.id + '.status.started', Math.round(Date.now()/1000));
-					library._setValue(device.id + '.status.startedDateTime', library.getDateTime(Date.now()));
+					library._setValue(device.id + '.job.started', Math.round(Date.now()/1000));
+					library._setValue(device.id + '.job.startedDateTime', library.getDateTime(Date.now()));
 					
-					library._setValue(device.id + '.status.finished', 0);
-					library._setValue(device.id + '.status.finishedDateTime', '');
+					library._setValue(device.id + '.job.finished', 0);
+					library._setValue(device.id + '.job.finishedDateTime', '');
 					
 					// voice output
 					if (device.alexa !== undefined && device.alexa !== '' && adapter.config.alexaStarted !== '')
@@ -168,9 +248,9 @@ adapter.on('stateChange', function(state, payload)
 					var endTime = Date.now();
 					
 					library._setValue(device.id + '._running', false);
-					library._setValue(device.id + '.status.finished', Math.round(endTime/1000));
-					library._setValue(device.id + '.status.finishedDateTime', library.getDateTime(endTime));
-					adapter.extendObject(device.id + '.status.average', {common: {metered: JSON.stringify([])}});
+					library._setValue(device.id + '.job.finished', Math.round(endTime/1000));
+					library._setValue(device.id + '.job.finishedDateTime', library.getDateTime(endTime));
+					adapter.extendObject(device.id + '.usage._average', {common: {metered: JSON.stringify([])}});
 					
 					// voice output
 					if (device.alexa !== undefined && device.alexa !== '' && adapter.config.alexaFinished !== '')
@@ -193,15 +273,17 @@ adapter.on('stateChange', function(state, payload)
 					{
 						// log job to history
 						jobs = JSON.parse(obj.val || '[]');
-						adapter.getState(device.id + '.status.total', function(err, obj)
+						adapter.getState(device.id + '.job.usage', function(err, obj)
 						{
 							var total = obj.val || 0;
-							adapter.getState(device.id + '.status.started', function(err, obj)
+							library._setValue(device.id + '.job.usage', 0);
+							
+							adapter.getState(device.id + '.job.started', function(err, obj)
 							{
 								var startTime = obj.val || 0;
 								jobs.push({
 									'total': total,
-									'runtime': startTime-Math.round(endTime/1000),
+									'runtime': Math.round(endTime/1000)-startTime,
 									'started': startTime,
 									'startedDateTime': library.getDateTime(startTime*1000),
 									'finished': Math.round(endTime/1000),
@@ -216,37 +298,44 @@ adapter.on('stateChange', function(state, payload)
 			});
 		});
 	});
-});
+}
 
 
 /**
  * Creates a device
  *
- * @param	{object}	device	Device to be added
+ * @param	{object}	device		Device to be added
  * @return	void
  *
  */
 function createDevice(device)
 {
-	// create states
+	// check if device is already created
 	device.id = device.id === undefined ? device.name.toLowerCase().replace(/ /g, '_') : device.id.toString();
-	adapter.createDevice(device.id, {}, {}, function()
+	adapter.getObject(device.id, function(err, obj)
 	{
-		// create state
-		nodes.forEach(function(node)
+		// device not created, so create states
+		if (err !== null || obj === null)
 		{
-			library.set({node: device.id + '.' + node.node, description: node.description, role: node.role, type: node.type}, '');
-		});
+			adapter.createDevice(device.id, {name: 'Device ' + device.name}, {}, function()
+			{
+				// create states
+				nodes.forEach(function(node)
+				{
+					library.set(Object.assign({}, node, {node: device.id + '.' + node.node}), node.role === 'value.power.consumption' ? 0 : '');
+				});
+				
+				// write log
+				adapter.log.info('Registered device ' + device.name + ' (' + device.state + ').');
+				
+				// extend monitored state
+				adapter.extendForeignObject(device.state, {common: {'meter-sockets': device}});
+			});
+		}
 		
-		// change name
-		adapter.extendObject(device.id, {common: {name: 'Device ' + device.name}});
-		
-		
-		// write log
-		adapter.log.info('Created device ' + device.name + ' (' + device.state + ').');
-		
-		// extend monitored state
-		adapter.extendForeignObject(device.state, {common: {'meter-sockets': device}});
+		// device already created, so change name
+		else
+			adapter.extendObject(device.id, {common: {name: 'Device ' + device.name}});
 	});
 	
 	return device;
